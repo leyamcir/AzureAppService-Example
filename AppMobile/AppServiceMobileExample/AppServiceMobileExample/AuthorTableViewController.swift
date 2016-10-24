e1e5a112abcbf516d6e775f351f22d4f8cfe4144
amdaza <amdazacastillo@gmail.com>
2016-10-24 23:06:44 +0200

    Delete Author Record

--- AppMobile/AppServiceMobileExample/AppServiceMobileExample/AuthorTableViewController.swift
+++ AppMobile/AppServiceMobileExample/AppServiceMobileExample/AuthorTableViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
+typealias AuthorRecord = Dictionary<String, AnyObject>
+
 class AuthorTableViewController: UITableViewController {
     
     var client: MSClient = MSClient(applicationURL: URL(string: "http://amdcboot3labs-mbaas.azurewebsites.net")!)
 
-    var model: [Dictionary<String, AnyObject>]? = []
+    var model: [AuthorRecord]? = []
     
     @IBAction func addNewAuthor(_ sender: AnyObject) {
         let alert = UIAlertController(title: "Nuevo Autor", message: "Escribe el nombre de un autor", preferredStyle: .alert)
@@ -23,7 +25,7 @@ class AuthorTableViewController: UITableViewController {
             let secondName = alert.textFields![1] as UITextField
             
             
-            self.insertNewAutor(nameAutor.text!, secondName: secondName.text!)
+            self.insertNewAuthor(name: nameAutor.text!, lastname: secondName.text!)
             
         }
         let actionCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
@@ -50,7 +52,7 @@ class AuthorTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         
-        addNewAuthor("Carlos Ruiz Zafón")
+        insertNewAuthor(name: "Carlos", lastname: "Ruiz Zafón")
         
         readAllItemsInTable()
     }
@@ -60,8 +62,9 @@ class AuthorTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    // MARK: - Insert in Authors table
-    func insertNewAutor(_ name: String) {
+    // MARK: - CRUD Authors table
+    
+    func insertNewAuthor(name: String, lastname: String) {
         let tableMS = client.table(withName: "Authors")
         
         tableMS.insert(["name": name, "lastname": lastname]) { (result, error) in
@@ -76,11 +79,27 @@ class AuthorTableViewController: UITableViewController {
         }
     }
     
+    func deleteAuthorRecord(item: AuthorRecord) {
+        
+        let tableMS = client.table(withName: "Authors")
+        
+        tableMS.delete(item) { (result, error) in
+            
+            if let _ = error {
+                print(error)
+                return
+            }
+            
+            // Refresh table
+            self.readAllItemsInTable()
+        }
+    }
+    
     func readAllItemsInTable() {
         let tableMS = client.table(withName: "Authors")
-        let predicate = NSPredicate(format: "name = 'Juan Martin'")
+      //  let predicate = NSPredicate(format: "name = 'Juan Martin'")
         
-        tableMS.read(with: predicate) { (results, error) in
+        tableMS.read { (results, error) in
             if let _ = error {
                 print(error)
                 return
@@ -123,17 +142,25 @@ class AuthorTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            tableView.beginUpdates()
             tableView.deleteRows(at: [indexPath], with: .fade)
+            let item = self.model?[indexPath.row]
+            
+            self.deleteAuthorRecord(item: item!)
+            self.model?.remove(at: indexPath.row)
+            
+            tableView.endUpdates()
+
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
